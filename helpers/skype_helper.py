9c505d8e84fed1acdc2c6097e13d0260c49512aa
shivaharip <shivahari@qxf2.com>
2022-02-01 20:06:18 +0530

    Added Base, SQS & Skype helper objects

---
+++ helpers/skype_helper.py
@@ -0,0 +1,35 @@
+"""
+Helper module for Skype
+"""
+import os
+import sys
+import requests
+
+# add project root to sys path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from helpers.base_helper import BaseHelper
+from conf import skype_conf
+
+class SkypeHelper(BaseHelper):
+    """
+    Skype Helper object
+    """
+
+    def post_message_on_skype(self, message):
+        "Posts a predefined message on the set Skype channel"
+        try:
+            headers = {'Content-Type': 'application/json'}
+            payload = {"msg" : message,
+                      "channel": os.environ['CHANNEL_ID'],
+                      "API_KEY": os.environ['API_KEY']}
+
+            response = requests.post(url=skype_conf.SKYPE_SENDER_ENDPOINT,
+                                     json=payload, headers=headers)
+            if response.status_code == 200:
+                self.write(f'Successfully sent the Skype message - {message}')
+            else:
+                self.write(f'Failed to send Skype message', level='error')
+        except Exception as err:
+            raise Exception(f'Unable to post message to Skype channel, due to {err}')
+
+        return response
