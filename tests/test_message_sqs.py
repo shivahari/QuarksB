a1bf4233f749fcc3fe1b18470732741b66a10225
rahul-bhave <rsbhave81@gmail.com>
2022-02-10 15:39:49 +0530

    url as argument ,time fix and proper naming for message sent

--- tests/test_message_sqs.py
+++ tests/test_message_sqs.py
@@ -3,8 +3,7 @@
   - Validate message sent to Skype channel against the message received on SQS
 """
 import time
-from conf import sqs_conf
-from conf import skype_conf
+from conf import sqs_conf, skype_conf
 
 def test_message_received_sqs(sqs_instance, skype_instance, concurrent_obj):
     """
@@ -14,15 +13,16 @@ def test_message_received_sqs(sqs_instance, skype_instance, concurrent_obj):
         with concurrent_obj.ThreadPoolExecutor() as executor:
             future = executor.submit(sqs_instance.get_message_from_queue, sqs_conf.SQS_NAME)
             # wait 3 secs before triggering Skype message
-            message = skype_conf.MESSAGE
+            message_sent = skype_conf.MESSAGE
+            url = skype_conf.SKYPE_SENDER_ENDPOINT
             time.sleep(3)
-            trigger_skype_message = skype_instance.post_message_on_skype(message)
+            trigger_skype_message = skype_instance.post_message_on_skype(message_sent, url)
             sqs_messages = future.result()
 
         # validate if message found
         if_message_found = False
         for msg in sqs_messages:
-            if message in msg:
+            if message_sent in msg:
                 if_message_found = True
                 break
         assert if_message_found
