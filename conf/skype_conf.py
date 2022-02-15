8203915b55175b35855c303f830b000993c0a85f
rahulbhave <55383777+rahul-bhave@users.noreply.github.com>
2022-02-15 15:31:52 +0530

    Update skype_conf.py

--- conf/skype_conf.py
+++ conf/skype_conf.py
@@ -4,4 +4,4 @@ Skype conf
 import time
 
 SKYPE_SENDER_ENDPOINT = "https://skype-sender.qxf2.com/send-message"
-MESSAGE = 'This is a test message - ' + ''.join(time.time().__str__().split('.'))
+MESSAGE = 'This is a test message - ' + ''.join(time.ctime().__str__().split('.'))
