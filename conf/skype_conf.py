4211d8da2f33cf441d9519713e7f471f0e622738
akkuldn <akkul.dn@qxf2.com>
2022-02-15 13:47:14 +0530

    Fixed review comment for Shivas PR

--- conf/skype_conf.py
+++ conf/skype_conf.py
@@ -2,6 +2,7 @@
 Skype conf
 """
 import random
+import time
 
 SKYPE_SENDER_ENDPOINT = "https://skype-sender.qxf2.com/send-message"
-MESSAGE = 'This is a test message - ' + ''.join(random.choices(['s', 'h', 'i', 'v', 'a']))
+MESSAGE = 'This is a test message %f'%time.time()
