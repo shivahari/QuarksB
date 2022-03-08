b9676e3ae00e68a6ba3d298d10fededdfea30b35
PreedhiVivek <preedhi.vivek@qxf2.com>
2022-03-08 13:11:30 +0530

    Moved conf to test

--- helpers/skype_helper.py
+++ helpers/skype_helper.py
@@ -4,32 +4,26 @@ Helper module for Skype
 import os
 import sys
 import requests
-
-# add project root to sys path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from helpers.base_helper import BaseHelper
-from conf import skype_conf
 
 class SkypeHelper(BaseHelper):
     """
     Skype Helper object
     """
-
-    def post_message_on_skype(self, message):
+    def post_message_on_skype(self, skype_message, skype_url):
         "Posts a predefined message on the set Skype channel"
         try:
             headers = {'Content-Type': 'application/json'}
-            payload = {"msg" : message,
+            payload = {"msg" : skype_message,
                       "channel": os.environ['CHANNEL_ID'],
                       "API_KEY": os.environ['API_KEY']}
 
-            response = requests.post(url=skype_conf.SKYPE_SENDER_ENDPOINT,
+            response = requests.post(url=skype_url,
                                      json=payload, headers=headers)
             if response.status_code == 200:
-                self.write(f'Successfully sent the Skype message - {message}')
+                self.write(f'Successfully sent the Skype message - {skype_message}')
             else:
                 self.write(f'Failed to send Skype message', level='error')
         except Exception as err:
             raise Exception(f'Unable to post message to Skype channel, due to {err}')
-
-        return response
+        return response, skype_message
