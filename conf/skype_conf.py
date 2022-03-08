256b6b19e16d6088582a01977fca318d763316c0
PreedhiVivek <preedhi.vivek@qxf2.com>
2022-03-08 13:11:05 +0530

    Update the Skype message with timestamp appended

--- conf/skype_conf.py
+++ conf/skype_conf.py
@@ -1,7 +1,7 @@
 """
 Skype conf
 """
-import random
+from datetime import datetime
 
 SKYPE_SENDER_ENDPOINT = "https://skype-sender.qxf2.com/send-message"
-MESSAGE = 'This is a test message - ' + ''.join(random.choices(['s', 'h', 'i', 'v', 'a']))
+MESSAGE = 'Test message sent on ' + datetime.now().strftime('%d-%m-%Y %H:%M:%S')
